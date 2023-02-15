ed2e0e41a4889e8f352e0f281ef5afb854013a41
Anderson Llanos <allanosq@unal.edu.co>
2023-02-14 21:18:25 -0500

    API-TESTING

---
+++ app/Models/Grupo.ts
@@ -0,0 +1,23 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Grupo extends BaseModel {
+
+  @column({isPrimary: true})
+  public codigo_grupo: number
+
+  @column()
+  public nombre_grupo: string
+
+  /*
+  @column({ isPrimary: true })
+  public id: number
+
+   */
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
---
+++ app/Models/Publicaciones.ts
@@ -0,0 +1,29 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Publicaciones extends BaseModel {
+
+  @column({isPrimary: true})
+  public codigo_publicacion: number
+
+  @column()
+  public codigo_usuario: number
+
+  @column()
+  public titulo: string
+
+  @column()
+  public cuerpo: string
+
+  /*
+  @column({ isPrimary: true })
+  public id: number
+
+   */
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
